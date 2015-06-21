89661efc2f521f294b68ad749e7d4d7285af527b
richardgarcia <richardgarciaph@gmail.com>
2015-06-21 18:45:21 +1200

    fixing form input validation

--- app/controllers/contacts.js
+++ app/controllers/contacts.js
@@ -3,8 +3,8 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
 	
 	
-	nameFromInput: ' ',
-	phoneFromInput: ' ',
+	// nameFromInput: ' ',
+	// phoneFromInput: ' ',
 
 	// nothingThere: Ember.computed.empty('nothingThere');
 	// fix this!!
