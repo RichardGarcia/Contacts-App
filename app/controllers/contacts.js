aefb5a3e6547c71e0e6ff5295d3df9dae04fdcbb
richardgarcia <richardgarciaph@gmail.com>
2015-07-04 20:26:39 +1200

    added other elements, observes, loading, etc..

--- app/controllers/contacts.js
+++ app/controllers/contacts.js
@@ -2,15 +2,45 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
 	
+	queryParams: ['isShowingPhoneNumber'],
 	
-	// nameFromInput: ' ',
-	// phoneFromInput: ' ',
+	nameFromInput: '',
+	phoneFromInput: '',
+	emailFromInput: '',
+	
+	// isNameEmpty: Ember.computed.empty('name'),
+	// isPhoneEmpty: Ember.computed.empty('phone'),
+	// nothingThere: Ember.computed.and('isNameEmpty', 'isPhoneEmpty'),
 
-	// nothingThere: Ember.computed.empty('nothingThere');
-	// fix this!!
-
+	isNameEmpty: Ember.computed.empty('nameFromInput'),
+	isPhoneEmpty: Ember.computed.empty('phoneFromInput'),
+		
+	nothingThere: Ember.computed.or('isNameEmpty', 'isPhoneEmpty'),
 
 	isShowingPhoneNumber: false,
+
+
+	contactsWithEmail: function() {
+		var model = this.get('model');
+		var filteredModel = model.filterBy('email');
+		return filteredModel.get('length');
+	}.property('model.@each'),
+
+	capitalName: function() {
+		var name = this.get('nameFromInput');
+		return name.toUpperCase();
+	}.property('nameFromInput'),
+
+	nameChanged: function() {
+		this.set('nameFromInput', this.get('capitalName'));
+	}.observes('nameFromInput'),
+
+
+	// do the dasherized function...
+
+	// check ember computed docs..
+	// check ember object model
+
 
 	actions: {
 	
@@ -20,28 +50,38 @@ export default Ember.Controller.extend({
 			this.toggleProperty('isShowingPhoneNumber');
 		},
 
-		submit() {
+		addNewContact: function() {
+
+			if (this.get('nothingThere')) {
+					return;
+				}
+
 			var newName = this.get('nameFromInput');
 			var newPhone = this.get('phoneFromInput');
+			var newEmail = this.get('emailFromInput');
+
 
 			// if (Ember.isBlank(newName) === '' || Ember.isBlank(newPhone) === '') {
 			// 	return;
 			// };
 
+
 			var newContact = this.store.createRecord('contact', {
 				name: newName,
-				phone: newPhone
+				phone: newPhone,
+				email: newEmail
 			});
 
 			newContact.save();
 
-			this.set('nameFromInput', '');
-			this.set('phoneFromInput', '');
+			this.set('name', '');
+			this.set('phone', '');
+			this.set('email', '');
 		},
 
-	deleteContact(item) {
-		item.destroyRecord();
-	}
+		deleteContact(item) {
+			item.destroyRecord();
+		}
 
 
 	}
